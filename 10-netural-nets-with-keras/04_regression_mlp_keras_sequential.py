e3a08ed66229b107a54796fbc28906d87dac38df
planning018 <13057588636@163.com>
2023-09-05 20:28:43 +0800

    add chapter02 MLP

---
+++ 10-netural-nets-with-keras/04_regression_mlp_keras_sequential.py
@@ -0,0 +1,39 @@
+import matplotlib.pyplot as plt
+import tensorflow as tf
+from packaging import version
+from sklearn.datasets import fetch_california_housing
+from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import train_test_split
+from sklearn.neural_network import MLPRegressor
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.datasets import load_iris
+from sklearn.neural_network import MLPClassifier
+
+assert version.parse(tf.__version__) >= version.parse("2.8.0")
+print("Tensorflow version: ", tf.__version__)
+
+housing = fetch_california_housing()
+X_train_full, X_test, y_train_full, y_test = train_test_split(housing.data, housing.target, random_state=42)
+X_train, X_valid, y_train, y_valid = train_test_split(X_train_full, y_train_full, random_state=42)
+
+tf.random.set_seed(42)
+norm_layer = tf.keras.layers.Normalization(input_shape=X_train.shape[1:])
+model = tf.keras.Sequential([
+    norm_layer,
+    tf.keras.layers.Dense(50, activation="relu"),
+    tf.keras.layers.Dense(50, activation="relu"),
+    tf.keras.layers.Dense(50, activation="relu"),
+    tf.keras.layers.Dense(1)
+])
+optimizer = tf.keras.optimizers.Adam(learning_rate=1e-3)
+model.compile(loss="mse", optimizer=optimizer, metrics=["RootMeanSquaredError"])
+norm_layer.adapt(X_train)
+history = model.fit(X_train, y_train, epochs=20,
+                    validation_data=(X_valid, y_valid))
+mse_test, rmse_test = model.evaluate(X_test, y_test)
+X_new = X_test[:3]
+y_pred = model.predict(X_new)
+
+print(rmse_test)
+print(y_pred)
